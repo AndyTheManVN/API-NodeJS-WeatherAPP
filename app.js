149d958a8b85e51b4c858bbba2c1baadd3362746
andydangVN <dangminhnghia@gmail.com>
2022-05-27 12:05:12 +0700

    Get data with https response from API get

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const https = require('https');
+
 const app = express();
 const port = (3000);
 
 // route: / (home) is index.html
 app.get('/', function(req, res){
-  
+  const url = 'https://api.openweathermap.org/data/2.5/weather?q=vientiane&appid=0127193beefea707db244b899e1051c7#';
+  https.get(url, function (response) {
+        console.log(response);
+  });
+
   res.sendFile(__dirname + "/index.html");
 });
 
