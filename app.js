11e8a0b8e85657407544bbcc9bca5d1b13f5c3f6
andydangVN <dangminhnghia@gmail.com>
2022-05-27 15:27:14 +0700

    Query data from get request

--- app.js
+++ app.js
@@ -4,11 +4,18 @@ const https = require('https');
 const app = express();
 const port = (3000);
 
-// route: / (home) is index.html
+// route: /  is index.html
 app.get('/', function(req, res){
   const url = 'https://api.openweathermap.org/data/2.5/weather?q=vientiane&appid=0127193beefea707db244b899e1051c7#';
   https.get(url, function (response) {
-        console.log(response);
+        // Xem status request
+        console.log(response.statusCode);
+        // Chuyển dữ liệu request từ API sang kiêu JSON để truy vấn
+        response.on("data", function(data){
+          const weatherData = JSON.parse(data);
+          const temp = weatherData.main.temp;
+          console.log(temp);
+        });
   });
 
   res.sendFile(__dirname + "/index.html");
